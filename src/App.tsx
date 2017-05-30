90348784e406a705b4e74ca630fd2cd51c89fa86
Duncan Mak <duncan.mak@microsoft.com>
2017-05-30 16:45:24 -0400

    Use react-router and add 2 routes for Foo and Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,56 @@
 import * as React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+import { Home, Foo, Bar } from './Components';
+
 import './App.css';
 
 const logo = require('./logo.svg');
 
 class App extends React.Component<{}, null> {
+  /*  
+    render() {
+      return (
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <h1>Hello</h1>
+        </div>
+      );
+    }
+  
+  */
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <div>
+            <p><Link to="/">Home</Link></p>
+            <p><Link to="/foo">Foo</Link></p>
+            <p><Link to="/bar">Bar</Link></p>
+          </div>
+          <hr />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/foo" component={Foo} />
+          <Route path="/bar" component={Bar} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <h1>Hello</h1>
-      </div>
-    );
+      </Router>);
   }
 }
 
